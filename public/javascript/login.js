95933ddee9c6d31d1fd2eb64fb2be0768e7d5242
cpesar <77510555+cpesar@users.noreply.github.com>
2021-06-20 18:18:29 -0600

    troubleshooting login route

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -35,6 +35,7 @@ async function loginFormHandler(event){
 
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
+  console.log(email, password)
 
   if (email && password){
     const response = await fetch('/api/users/login', {
